e3cd2b84112ee95ca2fa758df04ddbb2cc344358
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-24 15:17:15 +0000

    initial import acad_ue_cooluri
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/acad_ue/trunk@54807 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ acad_ue_cooluri/acad_ue_cooluri/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "acad_ue_cooluri".
+#
+# Auto generated 18-01-2010 14:58
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Cooluri Configuration',
+	'description' => 'Cooluri Configuration for the University of Erfurt',
+	'category' => 'fe',
+	'author' => 'Irene Höppner',
+	'author_email' => 'irene.hoeppner@abezet.de',
+	'shy' => '',
+	'dependencies' => 'cms,cooluri',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'cooluri' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"5320";s:15:"CoolUriConf.xml";s:4:"785c";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"90d7";s:20:"doc/wizard_form.html";s:4:"2549";}',
+	'suggests' => array(
+	),
+);
+
+?>
