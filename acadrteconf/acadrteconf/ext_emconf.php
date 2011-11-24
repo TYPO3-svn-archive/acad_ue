09c216b996d54e3103865fd57178ec83b6eed4fb
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-24 15:35:19 +0000

    initial import acadrteconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/acad_ue/trunk@54819 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ acadrteconf/acadrteconf/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "acadrteconf".
+#
+# Auto generated 19-01-2010 14:05
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Academic RTE Config',
+	'description' => 'Basic configuration of RTE with nearly all buttons, some classes, and, last but not least, adaptations of lib.parseFuncRTE',
+	'category' => 'misc',
+	'author' => 'Uschi Renziehausen',
+	'author_email' => 'ursula.renziehausen@uni-erfurt.de',
+	'shy' => '',
+	'dependencies' => 'rtehtmlarea,css_styled_content',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.9.0',
+	'constraints' => array(
+		'depends' => array(
+			'rtehtmlarea' => '',
+			'css_styled_content' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"4f64";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"eeb3";s:14:"ext_tables.php";s:4:"3d48";s:17:"locallang_rte.xml";s:4:"18b7";s:7:"rte.css";s:4:"a516";s:19:"x_locallang_rte.xml";s:4:"62bb";s:19:"doc/wizard_form.dat";s:4:"c6bb";s:20:"doc/wizard_form.html";s:4:"c3a3";s:15:"res/content.css";s:4:"d10c";s:11:"res/rte.css";s:4:"90f7";s:38:"res/images/symbols/background-info.png";s:4:"4aa7";s:26:"res/images/symbols/bad.png";s:4:"d8b4";s:27:"res/images/symbols/good.png";s:4:"35e8";s:27:"res/images/symbols/note.png";s:4:"1c7f";s:26:"res/images/symbols/tip.png";s:4:"3276";s:30:"res/images/symbols/warning.png";s:4:"a33f";s:20:"static/constants.txt";s:4:"a8e3";s:16:"static/setup.txt";s:4:"d742";s:25:"tsconfig/pageTSConfig.txt";s:4:"4531";s:33:"tsconfig/pageTSConfig_classes.txt";s:4:"ae68";s:30:"tsconfig/pageTSConfig_proc.txt";s:4:"c357";s:30:"tsconfig/pageTSConfig_user.txt";s:4:"d6a0";}',
+);
+
+?>
