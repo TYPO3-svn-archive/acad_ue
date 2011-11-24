d545023ef1e6b652be83e32ea284a4ebe1ecd95b
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-24 15:09:54 +0000

    initial import acad_rteconf_ue
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/acad_ue/trunk@54801 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ acad_rteconf_ue/acad_rteconf_ue/ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "acad_rteconf_ue".
+#
+# Auto generated 18-01-2010 14:57
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'RTE Uni Erfurt',
+	'description' => 'RTE adaptations specific for University of Erfurt based on Ext acadrteconf',
+	'category' => 'misc',
+	'author' => 'Uschi Renziehausen',
+	'author_email' => 'ursula.renziehausen@uni-erfurt.de',
+	'shy' => '',
+	'dependencies' => 'acadrteconf',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'acadrteconf' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:17:"ext_localconf.php";s:4:"e7bf";s:14:"ext_tables.php";s:4:"f75b";s:16:"static/setup.txt";s:4:"eeee";s:25:"tsconfig/pageTSConfig.txt";s:4:"4b68";}',
+);
+
+?>
