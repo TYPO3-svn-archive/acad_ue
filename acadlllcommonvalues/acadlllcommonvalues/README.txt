351054a054352a341bec1272876e814f8f3aa45b
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-24 15:33:01 +0000

    initial import acadlllcommonvalues
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/acad_ue/trunk@54816 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ acadlllcommonvalues/acadlllcommonvalues/README.txt
@@ -0,0 +1,6 @@
+Just a collection of some language values usefull on a website.
+- locallang_cwv.xml
+  Contains values for each and every project
+- locallang_uni_erfurt.xml
+  Special values for the University of Erfurt, should perhaps go out of here
+   	
---
+++ acadlllcommonvalues/acadlllcommonvalues/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "acadlllcommonvalues"
+#
+# Auto generated 11-02-2008 16:17
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Common Website Values',
+	'description' => 'A collection of multilingual terms useful for every website',
+	'category' => 'misc',
+	'author' => 'Uschi Renziehausen',
+	'author_email' => 'ursula.renziehausen@uni-erfurt.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"57de";s:10:"README.txt";s:4:"68a4";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"locallang_cwv.xml";s:4:"b6e0";s:24:"locallang_uni_erfurt.xml";s:4:"15fd";s:19:"doc/wizard_form.dat";s:4:"6a5a";s:20:"doc/wizard_form.html";s:4:"012d";}',
+);
+
+?>
