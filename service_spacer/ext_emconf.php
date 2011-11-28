5836da8f552cd4ede452717d443ebd87ab4d9c3f
rupi <rupi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-28 09:27:16 +0000

    initial import service_spacer
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/acad_ue/trunk@54990 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ service_spacer/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "service_spacer".
+#
+# Auto generated 15-11-2010 08:32
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Pagetype Service Spacer',
+	'description' => 'Adds a new doktype similar to spacer',
+	'category' => 'be',
+	'author' => 'Uschi Renziehausen',
+	'author_email' => 'ursula.renziehausen@uni-erfurt.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"8a41";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"b87a";s:14:"ext_tables.php";s:4:"6be2";s:13:"locallang.xml";s:4:"514f";s:18:"service-spacer.gif";s:4:"9155";s:19:"doc/wizard_form.dat";s:4:"d241";s:20:"doc/wizard_form.html";s:4:"38da";}',
+	'suggests' => array(
+	),
+);
+
+?>
